d6837bced224e1e8246ae4430ea111284382ab56
unknown <patryk1.stefan@gmail.com>
2020-10-07 19:09:29 +0200

    formik start:

--- oddam/src/components/App/index.js
+++ oddam/src/components/App/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Navigation from '../Navigation';
+import LandingPage from '../Page/LandingPage';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Page/HomePage';
+import AccountPage from '../Account';
+import AdminPage from '../Page/AdminPage';
 
+import * as ROUTES from '../constants/routes';
+import { withAuthentication } from '../Session';
 
-const App =()=> {
-return(
-        <p>test</p>
-)
-}
-export default App;
+const App = () => (
+    <Router>
+        <div>
+            <Navigation/>
+
+            <hr/>
+
+            <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+            <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+            <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
+            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+            <Route path={ROUTES.HOME} component={HomePage}/>
+            <Route path={ROUTES.ACCOUNT} component={AccountPage}/>
+            <Route path={ROUTES.ADMIN} component={AdminPage}/>
+        </div>
+    </Router>
+);
+
+export default withAuthentication(App);
